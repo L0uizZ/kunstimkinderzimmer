c2db6c5d0273345d9388f37817a100ca54f8fcef
L0uizZ <louis.hinz@hotmail.de>
2017-06-28 14:33:46 +0200

    Blog (Margins), Contactform (Layout), Artistfilter

--- src/ProjectBundle/Controller/ListController.php
+++ src/ProjectBundle/Controller/ListController.php
@@ -6,15 +6,30 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class ListController extends Controller
 {
+
+
     public function indexAction()
     {
         $repository = $this->getDoctrine()->getRepository('ProjectBundle:Author');
-        $dbcontent = $repository->findBy(array(),array('name' => 'ASC'));
+        $dbcontent = $repository;
+        $authors = $dbcontent->findBy(array(),array('name' => 'ASC'));
+
+        $letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        foreach(str_split($letters) as $letter) {
+            $firstLetter = $letter;
+        }
+        $query = $this->createQueryBuilder('n');
+        $query->andWhere('n.name LIKE :firstLetter');
+        $query->setParameter('firstLetter', $firstLetter.'%');
+        return $query->getQuery()->getResult();
+
+
 
         if (!$dbcontent) {
             throw new \UnexpectedValueException('No Artist found in DB');
         } else {
-            return $this->render('ProjectBundle:Default:authorlist.html.twig', array('authors' => $dbcontent));
+            return $this->render('ProjectBundle:Default:authorlist.html.twig', array('authors' => $string));
         }
     }
 
