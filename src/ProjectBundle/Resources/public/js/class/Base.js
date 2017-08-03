d04c403d244a70c068dc34e5496e72ecb1aa526e
Nico Pakai <nicolas.pakai@xq-web.de>
2017-08-03 18:09:57 +0200

    book single page desktop layout

--- src/ProjectBundle/Resources/public/js/class/Base.js
+++ src/ProjectBundle/Resources/public/js/class/Base.js
@@ -5,7 +5,15 @@ function Base() {
   this.init = function() {
     self.handleMenu();
     self.initSlider();
+    self.collapseText();
   };
+
+  this.collapseText = function() {
+    $('.book-page .content-grid .title').on('click', function() {
+      $(this).next('div').find('[data-grid-text]').toggle();
+      $(this).toggleClass('open');
+    });
+  }
 
   this.initSlider = function() {
     $('[data-item-slider]').slick({
@@ -31,6 +39,16 @@ function Base() {
         }
       ]
     });
+
+    $('[data-book-slider]').slick({
+      infinite: true,
+      prevArrow: '<div class="slider-arrow-left"></div>',
+      nextArrow: '<div class="slider-arrow-right"></div>',
+      dots: true,
+      slidesToShow: 1,
+      adaptiveHeight: true
+    });
+
   }
 
   this.handleMenu = function () {
